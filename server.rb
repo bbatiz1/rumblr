a081011bae45944f33a782a5300f7491f3efed7f
bbatiz1 <batizbrandon1@gmail.com>
2020-01-02 00:29:18 -0500

    forms are set up, wrking db!

---
+++ server.rb
@@ -0,0 +1,55 @@
+require 'sinatra/activerecord'
+require 'sinatra'
+require 'sinatra/flash'
+require './models'
+
+set :port, 3000
+set :database, {adapter: "sqlite3", database: "users.sqlite3"}
+enable :sessions
+
+get '/' do
+  erb :home
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  user = User.find_by(email: params[:email])
+  given_password = params[:password]
+  if user.password == given_password
+    session[:user_id] = user.id
+    redirect '/profile'
+  else
+    flash[:error] = "Correct email, but wrong password."
+    redirect "/login"
+  end
+end
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  @user = User.new(params[:user])
+  if @user.valid?
+  @user.save
+  redirect '/profile'
+else
+  flash[:error] = @user.errors.full_message
+  redirect '/signup'
+end
+  p params
+end
+
+get '/profile' do
+  erb :profile
+end
+
+get '/logout' do
+  erb :logout
+end
+get '/logout' do
+  session.clear
+  redirect '/login'
+end
